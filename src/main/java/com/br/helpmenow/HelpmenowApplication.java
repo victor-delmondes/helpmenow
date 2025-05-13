08eb5199c0933a81b2135112893bd3fc30193a3f
Victor Delmondes <v.emanuel2013@gmail.com>
2025-05-13 10:39:04 -0300

    primeiro commit

---
+++ src/main/java/com/br/helpmenow/HelpmenowApplication.java
@@ -0,0 +1,13 @@
+package com.br.helpmenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpmenowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpmenowApplication.class, args);
+	}
+
+}
