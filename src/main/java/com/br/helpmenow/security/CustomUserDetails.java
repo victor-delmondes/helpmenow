eed1d5fab7b3d23302cab09f405c3c8be07cffb1
Victor Delmondes <v.emanuel2013@gmail.com>
2025-05-17 13:55:58 -0300

    correção no login

--- src/main/java/com/br/helpmenow/security/CustomUserDetails.java
+++ src/main/java/com/br/helpmenow/security/CustomUserDetails.java
@@ -31,6 +31,26 @@ public class CustomUserDetails implements UserDetails {
         return userApp.getEmail();
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return userApp.isActive();
+    }
+
     public UserApp getUser() {
         return userApp;
     }
